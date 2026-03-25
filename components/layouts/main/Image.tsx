faa4b00c2e95d7e1a35ee8233aa82096ef639ef1
Your Name <you@example.com>
2026-03-25 21:38:41 +0900

    fixed

--- components/layouts/main/Image.tsx
+++ components/layouts/main/Image.tsx
@@ -9,7 +9,7 @@ export const ImageLayout = ({ file_name, alt }: Props) => {
   return (
     <div className="w-full h-full relative overflow-clip">
       <Image
-        src={`${process.env.NEXT_PUBLIC_BASE_PATH}/${file_name}`}
+        src={`/images/${file_name}`}
         alt={alt}
         fill
         className="object-contain"

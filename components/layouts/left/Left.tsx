2817f45a637c71832fc6dda6fffeedc57ee16e79
Your Name <you@example.com>
2026-04-03 18:16:23 +0900

    video追加

--- components/layouts/left/Left.tsx
+++ components/layouts/left/Left.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect } from "react";
 
 export const LeftLayout = () => {
-  return <div className="hidden lg:block flex-1 bg-blue-300">LeftLayout</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.75;
+    }
+  }, []);
+
+  return (
+    <div className="hidden lg:block flex-1 relative overflow-hidden">
+      <video
+        ref={videoRef}
+        src="/images/left.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="inset-0 w-full h-full object-cover"
+      />
+    </div>
+  );
 };
--- components/layouts/right/Right.tsx
+++ components/layouts/right/Right.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect } from "react";
 
 export const RightLayout = () => {
-  return <div className="hidden lg:block flex-1 bg-red-300">RightLayout</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.75;
+    }
+  }, []);
+
+  return (
+    <div className="hidden lg:block flex-1 relative overflow-hidden">
+      <video
+        ref={videoRef}
+        src="/images/left.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="inset-0 w-full h-full object-cover"
+      />
+    </div>
+  );
 };
